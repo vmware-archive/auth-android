8f785316f577543fcf23c037dd6a7cab7eb6cfc2
Gavin Enns and Joshua Winters <genns@pivotal.io>
2015-07-30 15:03:50 -0400

    Customizes socket factory used for ssl validation

--- auth/src/main/java/io/pivotal/android/auth/NetHttpTransportBuilder.java
+++ auth/src/main/java/io/pivotal/android/auth/NetHttpTransportBuilder.java
@@ -4,11 +4,16 @@
 package io.pivotal.android.auth;
 
 import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.util.SslUtils;
 
 import org.apache.http.conn.ssl.StrictHostnameVerifier;
 
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
 
 public class NetHttpTransportBuilder {
 
@@ -24,9 +29,16 @@ public class NetHttpTransportBuilder {
     }
 
     public NetHttpTransportBuilder trustCertificates(final KeyStore keyStore) throws GeneralSecurityException {
-        mBuilder.trustCertificates(keyStore);
+        mBuilder.setSslSocketFactory(getSslSocketFactory(keyStore));
         mBuilder.setHostnameVerifier(new StrictHostnameVerifier());
         return this;
+    }
+
+    private SSLSocketFactory getSslSocketFactory(final KeyStore keyStore) throws GeneralSecurityException {
+        final SSLContext sslContext = SslUtils.getTlsSslContext();
+        final TrustManagerFactory trustFactory = SslUtils.getDefaultTrustManagerFactory();
+        SslUtils.initSslContext(sslContext, keyStore, trustFactory);
+        return sslContext.getSocketFactory();
     }
 
     public NetHttpTransport build() {
